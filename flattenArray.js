580fc048df79934dae6b24077fb335a19c5ac329
Bartek Maksimowski <maksimowski97@gmail.com>
2023-04-12 14:45:23 +0200

    new exercise

---
+++ flattenArray.js
@@ -0,0 +1,38 @@
+/*
+* Cel zadania
+*------------
+* Weź tablicę zawierającą zagnieżdżone tablice i zwróć płaską tablicę zawierającą  wartości liczbowe bez powtórek oraz pozbawioną wartości null/undefined.
+*
+* Przykładowo: 
+* flattenArray([4, [3, 2, undefined, 1], [1, 4, null, 5]]) => [4, 3, 2, 1, 5]
+* 
+* Oczekuj tylko jednego poziomu zagnieżdżenia tablic.
+* 
+*/
+
+function flattenArray(deepArray) {
+    const result = [];
+    
+    deepArray.flat().filter(el => {
+      if (el !== undefined && el !== null && !result.includes(el)) {
+        result.push(el)
+      }
+    })
+    return result.join(', ')
+  }
+  
+  
+  /* Weryfikacja */
+  
+  function verify(input, goal) {
+    input = Array.isArray(input) ? input.join(', ') : input;
+    if (input == goal) {
+      console.log('Gratulacje!');
+    } else {
+      console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+    }
+  }
+  
+  verify(flattenArray([4, [3, 2, undefined, 1], [1, 4, null, 5]]), "4, 3, 2, 1, 5");
+  verify(flattenArray([null, [1, 2, 3], [null, undefined]]), "1, 2, 3");
+  
