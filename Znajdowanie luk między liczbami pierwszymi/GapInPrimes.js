47019fb82bdb0818d4e2f6d8289c2925376c1958
Bartek Maksimowski <maksimowski97@gmail.com>
2023-05-20 12:54:39 +0200

    GapInPrime exercise added

--- Znajdowanie luk między liczbami pierwszymi/GapInPrimes.js
+++ Znajdowanie luk między liczbami pierwszymi/GapInPrimes.js
@@ -17,7 +17,7 @@ function gap(g, m, n) {
     return null;
   }
   
-  function isPrime(num) {
+function isPrime(num) {
     if (num <= 1) {
       return false;
     }
@@ -27,9 +27,9 @@ function gap(g, m, n) {
       }
     }
     return true;
-  }
-console.log(gap(2,100,110))
-console.log(gap(4,100,110))
-console.log(gap(6,100,110))
-console.log(gap(8,300,400))
-console.log(gap(10,300,400))
+}
+console.log(gap(2,100,110)) // [101, 103]
+console.log(gap(4,100,110)) // [103, 107]
+console.log(gap(6,100,110)) // null
+console.log(gap(8,300,400)) // [359, 367]
+console.log(gap(10,300,400)) // [337, 347]
